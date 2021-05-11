aeb59250c2c91d10e0aaa56f397da0c4590e67c6
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-05-11 14:26:44 +0300

    feat: add row and column

---
+++ src/components/Column/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const Column: FC = ({ children }) => (
+  <td>
+    {children}
+  </td>
+);
+
+export default Column;
---
+++ src/components/Row/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const Row: FC = ({ children }) => (
+  <tr>
+    {children}
+  </tr>
+);
+
+export default Row;
